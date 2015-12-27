c1c5d47db0cfc26b3b4600289db362caa8617292
Joseph Sayad <JosephSayad95@gmail.com>
2015-12-27 15:58:03 -0500

    Initial commit: Added get-inspired functionality

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
+$(document).ready(function() {
+
+  $('.unanswered-getter').submit(function(event){
+	event.preventDefault();
+	// zeros out results container if previous search has run
+	$('.results').html(''); 
+	// get the value of the tags the user submitted
+	var tags = $(this).find("input[name='tags']").val();
+	getUnanswered(tags);
+  });
+
+  $('.inspiration-getter').submit(function(event) {
+    event.preventDefault();
+    $('.results').html(''); 
+    var answerers = $(this).find("input[name='answerers']").val();
+    getInspired(answerers);
+  });
+
+});
+
 // this function takes the question object returned by the StackOverflow request
 // and returns new result to be appended to DOM
 var showQuestion = function(question) {
@@ -31,6 +51,27 @@ var showQuestion = function(question) {
 	return result;
 };
 
+// this function takes the answerer object returned by the StackOverflow request
+// and returns new result to be appended to DOM
+var showAnswerer = function(answerer) {  
+
+  // clone our result template code
+  var result = $('.templates .top-answerer').clone();
+
+  // Set the name property in result
+  var nameElem = result.find('.answerer-name a');
+  nameElem.attr('href', answerer.user.link);
+  nameElem.text(answerer.user.display_name);
+  
+  // Set the reputation property in result
+  var points = result.find('.answerer-reputation-points');
+  points.text('Reputation: ' + answerer.user.reputation);
+
+  var profilePicture = result.find('.answerer-profile-thumbnail');
+  profilePicture.attr('src', answerer.user.profile_image);
+
+  return result;
+}
 
 // this function takes the results object from StackOverflow
 // and returns the number of results and tags to be appended to DOM
@@ -49,8 +90,7 @@ var showError = function(error){
 // takes a string of semi-colon separated tags to be searched
 // for on StackOverflow
 var getUnanswered = function(tags) {
-	
-	// the parameters we need to pass in our request to StackOverflow's API
+	// the parameters we need to pass in our request to the StackExchange API
 	var request = { 
 		tagged: tags,
 		site: 'stackoverflow',
@@ -59,12 +99,13 @@ var getUnanswered = function(tags) {
 	};
 	
 	$.ajax({
-		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		url: 'http://api.stackexchange.com/2.2/questions/unanswered',
 		data: request,
-		dataType: "jsonp",//use jsonp to avoid cross origin issues
-		type: "GET",
+		dataType: 'jsonp', //use jsonp to avoid cross origin issues
+		type: 'GET',
 	})
-	.done(function(result){ //this waits for the ajax to return with a succesful promise object
+	//this waits for the ajax to return with a succesful promise object
+	.done(function(result) {
 		var searchResults = showSearchResults(request.tagged, result.items.length);
 
 		$('.search-results').html(searchResults);
@@ -75,20 +116,38 @@ var getUnanswered = function(tags) {
 			$('.results').append(question);
 		});
 	})
-	.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
+	//this waits for the ajax to return with an error promise object
+	.fail(function(jqXHR, error) {
 		var errorElem = showError(error);
 		$('.search-results').append(errorElem);
 	});
 };
 
-
-$(document).ready( function() {
-	$('.unanswered-getter').submit( function(e){
-		e.preventDefault();
-		// zero out results if previous search has run
-		$('.results').html('');
-		// get the value of the tags the user submitted
-		var tags = $(this).find("input[name='tags']").val();
-		getUnanswered(tags);
+var getInspired = function(tag) {
+  // the parameters we need to pass in our request to the StackExchange API
+  var request = {
+    site: 'stackoverflow'
+  };
+  
+  $.ajax({
+    url: 'http://api.stackexchange.com/2.2/tags/'+ tag +'/top-answerers/all_time',
+	data: request,
+	dataType: 'jsonp', 
+	type: 'GET',
+  })
+  .done(function(result) {
+  	console.log(result);
+    var searchResults = showSearchResults(tag, result.items.length); 
+    $('.search-results').html(searchResults);
+    
+    $.each(result.items, function(i, item) {
+	  var topAnswerer = showAnswerer(item);
+	  $('.results').append(topAnswerer);
 	});
-});
+
+  })
+  .fail(function(jqXHR, error) {
+	var errorElem = showError(error);
+	$('.search-results').append(errorElem);
+  });
+};
